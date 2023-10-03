10e30425176589399e9e2318fc8cd38c395af436
Bharat Prajapati <53166316+Bharat2912@users.noreply.github.com>
2023-10-03 18:01:27 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const data = [
-    { id: 1, name: 'Bharat here' },
+    { id: 1, name: 'Bharat here hello' },
     { id: 2, name: 'Item 2 from Service' },
     { id: 3, name: 'Item 3 from Service' }
 ];
